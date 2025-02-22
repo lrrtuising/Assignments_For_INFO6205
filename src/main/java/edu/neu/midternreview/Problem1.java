9952f4b6bcafcf077ba627994309bce01e942bbe
lrrtuising <lrrtuising@outlook.com>
2025-02-21 22:18:06 -0800

    feat: midtern review

---
+++ src/main/java/edu/neu/midternreview/Problem1.java
@@ -0,0 +1,57 @@
+package edu.neu.midternreview;/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Rushdi
+ */
+
+import java.util.*;
+
+public class Problem1 {
+    
+    // IMPLEMENT THE BELOW FUNCTION
+    public static List<List<Integer>> permute(int[] nums) {
+        
+        // UPDATE THIS RETURN STATEMENTS ALSO!!!!
+        List<List<Integer>> result = new ArrayList<>();
+        List<Integer> current = new ArrayList<>();
+        boolean[] used = new boolean[nums.length];
+        backtrack(result, current, nums, used);
+        return result;
+    }
+
+    private static void backtrack(List<List<Integer>> result, List<Integer> current, int[] nums, boolean[] used) {
+        if (current.size() == nums.length) {
+            result.add(new ArrayList<>(current));
+            return;
+        }
+
+        for (int i = 0; i < nums.length; i++) {
+            if (used[i]) continue;
+            
+            used[i] = true;
+            current.add(nums[i]);
+            backtrack(result, current, nums, used);
+            used[i] = false;
+            current.remove(current.size() - 1);
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] nums1 = {1, 2, 3};
+        System.out.println("Permutations of [1, 2, 3]: " + permute(nums1));
+        
+        int[] nums2 = {0, 1};
+        System.out.println("Permutations of [0, 1]: " + permute(nums2));
+        
+        int[] nums3 = {1};
+        System.out.println("Permutations of [1]: " + permute(nums3));
+        
+        int[] nums4 = {4, 5, 6, 7};
+        System.out.println("Permutations of [4, 5, 6, 7]: " + permute(nums4));
+    }
+}
+
---
+++ src/main/java/edu/neu/midternreview/Problem4.java
@@ -0,0 +1,60 @@
+package edu.neu.midternreview;/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Rushdi
+ */
+import java.util.Stack;
+
+public class Problem4 {
+
+    // IMPLEMENT THE BELOW FUNCTION!!
+    public static String removeDuplicateLetters(String s) {
+        boolean[] inResult = new boolean[26];
+
+        int[] lastIndex = new int[26];
+
+        for (int i = 0; i < s.length(); i++) {
+            lastIndex[s.charAt(i) - 'a'] = i;
+        }
+    
+        Stack<Character> stack = new Stack<>();
+    
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (inResult[c - 'a']) continue;
+        
+            while (!stack.isEmpty() && c < stack.peek() && i < lastIndex[stack.peek() - 'a']) {
+                inResult[stack.pop() - 'a'] = false;
+            }
+        
+            stack.push(c);
+            inResult[c - 'a'] = true;
+        }
+    
+        StringBuilder result = new StringBuilder();
+        for (char c : stack) result.append(c);
+        return result.toString();
+    }
+
+    public static void main(String[] args) {
+        // Test cases
+        String[] testCases = {
+            "bcabc",        // Expected output: "abc"
+            "cbacdcbc",     // Expected output: "acdb"
+            "aaaa",         // Expected output: "a"
+            "abacb",        // Expected output: "abc"
+            "ecbacba",      // Expected output: "abc"
+            "cdadabcc"      // Expected output: "adbc"
+        };
+        
+        // Test the function with all test cases
+        for (String testCase : testCases) {
+            System.out.println("Input: " + testCase + " -> Output: " + removeDuplicateLetters(testCase));
+        }
+    }
+}
+
